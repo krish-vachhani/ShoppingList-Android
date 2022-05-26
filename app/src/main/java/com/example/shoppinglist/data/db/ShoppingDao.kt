03283a8eded8c703b9a707381311ef3cb77af48e
Krish Vachhani <krishvachhani19@gmail.com>
2022-05-26 09:45:46 +0530

    initial commit

---
+++ app/src/main/java/com/example/shoppinglist/data/db/ShoppingDao.kt
@@ -0,0 +1,19 @@
+package com.example.shoppinglist.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.shoppinglist.data.db.entities.ShoppingItem
+
+@Dao
+interface ShoppingDao {
+
+    //mixture of update and insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item: ShoppingItem)
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>>
+}
